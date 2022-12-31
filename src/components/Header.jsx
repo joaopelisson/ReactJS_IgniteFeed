5b461231a7cafafa2a59757de391c158bf27151a
joao.pelisson <joaopedro.devbr@gmail.com>
2022-12-30 21:52:25 -0300

    CSS Modules

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import styles from  './Header.module.css'
+export function Header(){
+    return (
+        <header className={styles.header}>
+            <strong >
+                Olá mundo! strong
+            </strong>
+        </header>
+    )
+}
