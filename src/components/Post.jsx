2e9188b01d17b3dbe8e94c00d3a29b8f431f47c6
joao.pelisson <joaopedro.devbr@gmail.com>
2023-01-08 20:29:01 -0300

    Adding comment (useState)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,13 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR'
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import { Comment } from './Comment'
 import styles from './Post.module.css';
 
 export function Post({author, publishedAt, content}){
+    const [commentsMock, setcommentsMock] = useState([1,2]);
+
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'ás' HH:mm'h'", {
         locale: ptBR,
     });
@@ -13,6 +16,11 @@ export function Post({author, publishedAt, content}){
         locale: ptBR,
         addSuffix: true,
     });
+
+    function handleCreateNewComment() {
+        event.preventDefault();
+        setcommentsMock([...commentsMock, commentsMock.length + 1]);
+    }
 
     return (
         <article className={styles.post}>
@@ -44,7 +52,7 @@ export function Post({author, publishedAt, content}){
                 }
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
                 <textarea
                     placeholder='Deixe um comentário'
@@ -56,9 +64,13 @@ export function Post({author, publishedAt, content}){
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+             {
+                commentsMock.map(comment => {
+                    return (
+                        <Comment />
+                    )
+                })
+             }
             </div>
         </article>
     )
