fc0742f13848a8719acadc4e41329c0d80834e09
joao.pelisson <joaopedro.devbr@gmail.com>
2023-01-08 22:01:05 -0300

    Validating form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -28,9 +28,12 @@ export function Post({author, publishedAt, content}){
     }
 
     function handleNewCommentChange(){
+        event.target.setCustomValidity(``);
         setNewCommentText(event.target.value);
     }
-
+    function handleNewCommentInvalid(){
+        event.target.setCustomValidity(`Esse campo é obrigatório!`);
+    }
     function deleteComment(commentToDelete){
         
         const commentsWithoutDeletedOne = commentsMock.filter(comment => {
@@ -39,6 +42,9 @@ export function Post({author, publishedAt, content}){
 
         setcommentsMock(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
+
     return (
         <article className={styles.post}>
             <header>
@@ -80,10 +86,13 @@ export function Post({author, publishedAt, content}){
                     placeholder='Deixe um comentário'
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button
+                        type='submit' disabled={isNewCommentEmpty}>Publicar</button>
                 </footer>
             </form>
 
